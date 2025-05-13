b07dba0d1adbd43a8b08601c7339eaae5b6ef9c3
o2003shin <o2003shin@gmail.com>
2025-05-13 15:47:28 -0700

    Set up UI and state for resume and job description input

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import resumeLogo from './assets/paper-logo.png'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ resume, setResume ] = useState<string>('')
+  const [ jobDesc, setJobDesc ] = useState<string>('')  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="logo-title">
+        <img src={resumeLogo} className="logo resume" alt="Resume logo" />
+        <h1>Resume Match</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="resume-job-input">
+        <div className="text-boxes">
+          <section className="user-text">
+            <div className="resume">
+              <label>
+                <h4>Paste your resume</h4>
+                <textarea placeholder='Resume' 
+                          defaultValue={''} 
+                          value={resume} 
+                          onChange={e => setResume(e.target.value)}/>
+              </label>
+            </div>
+            <div className="job-description">
+              <label>
+                <h4>Paste your job description</h4>
+                <textarea placeholder='Job description' 
+                          defaultValue={''} 
+                          value={jobDesc}
+                          onChange={e => setJobDesc(e.target.value)}/>
+              </label>
+            </div>
+          </section>
+          <button>Analyze</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
